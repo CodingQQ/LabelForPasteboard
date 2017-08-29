c108bbaf9715e26e5af6abc17fc1d930cf9d61aa
LeeQQ <server@mycloudedu.com.cn>
2017-08-29 16:05:57 +0800

    label

--- LabelForPasteboard/LabelForPasteboard.h
+++ LabelForPasteboard/LabelForPasteboard.h
@@ -4,7 +4,7 @@
 //
 //  Created by LeeQQ on 2017/6/28.
 //  Copyright © 2017年 LeeQQ. All rights reserved.
-//
+// https://github.com/CodingQQ/LabelForPasteboard.git
 
 #import <UIKit/UIKit.h>
 /** 可以 复制、粘贴的label */
